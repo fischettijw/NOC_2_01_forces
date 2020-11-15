5a8ffe1c0bac53f68ee3ea317b24f7b15d66ebb8
Joseph Fischetti <fischettijw@gmail.com>
2020-11-15 16:15:10 -0500

    initial

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let mover;
+
+function setup() {
+    createCanvas(640, 360);
+    mover = new Mover();
+    createP('Click mouse to apply wind force.');
+}
+
+function draw() {
+    background(51);
+
+    let gravity = createVector(0, 0.1);
+    mover.applyForce(gravity);
+
+    if (mouseIsPressed) {
+        let wind = createVector(0.1, 0);
+        mover.applyForce(wind);
+    }
+
+    mover.update();
+    mover.display();
+    mover.checkEdges();
+}
